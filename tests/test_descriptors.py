0103df216f660311f24c354f562dbf6f75ff4c15
Antoine Poinsot <darosior@protonmail.com>
2022-04-28 12:06:48 +0200

    qa: cleanup descriptors tests

--- tests/test_descriptors.py
+++ tests/test_descriptors.py
@@ -23,13 +23,11 @@ from miniscript.miniscript.satisfaction import SatisfactionMaterial
 from miniscript.utils.hashes import sha256
 
 
-def sat_test(
+def sign_dummy_tx(
     descriptor,
-    keypairs={},
-    sat_material=None,
+    keypairs,
 ):
-    """Test a descriptor's satisfaction against libbitcoinconsensus."""
-    # Create a dummy spending transaction
+    """Create and sign a dummy transaction with the given keys."""
     amount = 10_000
     txid = bytes.fromhex(
         "652c60ec08280356e8c78be9bf4d44276acef3189ba8223e426b757aeabd66ad"
@@ -38,8 +36,6 @@ def sat_test(
     txout = CMutableTxOut(amount - 1_000, descriptor.script_pubkey)
     tx = CMutableTransaction([txin], [txout])
 
-    # Now populate the "signing data". Tell the satisfier about the available timelocks
-    # and signatures. Since we'll check them, produce valid sigs for the dummy tx.
     sighash = RawBitcoinSignatureHash(
         script=descriptor.script_sighash,
         txTo=tx,
@@ -53,22 +49,19 @@ def sat_test(
         sig = coincurve.PrivateKey(privkey).sign(sighash, hasher=None)
         signatures[pubkey] = sig + b"\x01"  # SIGHASH_ALL
 
-    # Pretty hacky but hey
-    if sat_material is not None:
-        sat_material.signatures = signatures
-        stack = descriptor.satisfy(sat_material)
-    else:
-        stack = descriptor.satisfy(list(signatures.values())[0])
+    return tx, signatures, amount
 
-    # Finally check it against libbitcoinconsensus. Note this is missing Taproot's flags
-    # but we don't care as we only support P2WSH for now.
+
+def verify_tx(descriptor, tx, witness_stack, amount):
+    """Test a transaction's first input spending a given descriptor against libbitcoinconsensus."""
     ConsensusVerifyScript(
-        scriptSig=txin.scriptSig,
+        scriptSig=tx.vin[0].scriptSig,
         scriptPubKey=CScriptBitcoinTx(iter(descriptor.script_pubkey)),
         txTo=tx,
         inIdx=0,
         amount=amount,
-        witness=CScriptWitness(stack),
+        witness=CScriptWitness(witness_stack),
+        # NOTE: that's missing Taproot flags
         flags=BITCOINCONSENSUS_ACCEPTED_FLAGS,
     )
 
@@ -82,18 +75,23 @@ def test_wsh_sanity_checks():
 
     desc_str = f"wsh(and_b(pk({pubkey.hex()}),a:sha256({digest.hex()})))"
     desc = Descriptor.from_str(desc_str)
+
     sat_material = SatisfactionMaterial(preimages={digest: preimage})
-    sat_test(
-        desc,
-        keypairs={pubkey: privkey},
-        sat_material=sat_material,
-    )
+    tx, signatures, amount = sign_dummy_tx(desc, keypairs={pubkey: privkey})
+    sat_material.signatures = signatures
+
+    stack = desc.satisfy(sat_material)
+    verify_tx(desc, tx, stack, amount)
 
 
 def test_wpkh_sanity_checks():
     """Sanity check we can parse a wpkh descriptor and satisfy it."""
     hd = BIP32.from_seed(os.urandom(32))
     pubkey, privkey = hd.get_pubkey_from_path("m"), hd.get_privkey_from_path("m")
+
     desc_str = f"wpkh({pubkey.hex()})"
     desc = Descriptor.from_str(desc_str)
-    sat_test(desc, keypairs={pubkey: privkey})
+
+    tx, signatures, amount = sign_dummy_tx(desc, keypairs={pubkey: privkey})
+    stack = desc.satisfy(list(signatures.values())[0])
+    verify_tx(desc, tx, stack, amount)
