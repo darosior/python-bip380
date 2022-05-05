7dc7d8b81466f44213b4403a0aec9acd2593c3a8
stickies-v <stickies-v@protonmail.com>
2022-05-05 12:25:25 +0200

    Add MiniscriptMalformed exception
    Replaces the generic Exception that's currently thrown

--- bip380/miniscript/errors.py
+++ bip380/miniscript/errors.py
@@ -3,6 +3,11 @@ All the exceptions raised when dealing with Miniscript.
 """
 
 
+class MiniscriptMalformed(ValueError):
+    def __init__(self, message):
+        self.message = message
+
+
 class MiniscriptNodeCreationError(ValueError):
     def __init__(self, message):
         self.message = message
